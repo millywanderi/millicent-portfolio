82b4f4ad07b8c953d9bec632b14269f38a0721b7
millywanderi <wanderinyambura5@gmail.com>
2026-07-03 14:02:23 +0300

    Mapped the experience page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Layout from "./components/layout/Layout";
 import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
+import Experience from "./components/sections/Experience";
 
 function App() {
   return (
     <Layout>
       <Hero />
       <About />
+      <Experience />
     </Layout>
   );
 }
