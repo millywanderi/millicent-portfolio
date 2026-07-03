8917eb432a458ffadd86e6c23a9c3f2375b38d0b
millywanderi <wanderinyambura5@gmail.com>
2026-07-03 14:53:58 +0300

    Added the Home navigation

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -28,6 +28,12 @@ const Navbar = () => {
             <ul className="hidden md:flex items-center gap-8 font-medium text-gray-700 dark:text-gray-200">
 
               <li>
+                <a href="#home" className="hover:text-blue-700 transition">
+                  Home
+                </a>
+              </li>
+
+              <li>
                 <a href="#about" className="hover:text-blue-700 transition">
                   About
                 </a>
