8fd0969b797e46bf319f9e4921b18b692c4b2010
millywanderi <wanderinyambura5@gmail.com>
2026-07-03 14:08:05 +0300

    Mapped the projects page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Layout from "./components/layout/Layout";
 import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
 import Experience from "./components/sections/Experience";
+import Projects from "./components/sections/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
     </Layout>
   );
 }
